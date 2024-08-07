a9059d87b9573836df8ee645569bdf804fbf9be8
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-08-07 20:33:34 +0000

    Finished my first CS 162 assignment

--- hw-intro/limits.c
+++ hw-intro/limits.c
@@ -2,9 +2,12 @@
 #include <sys/resource.h>
 
 int main() {
-    struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
-    return 0;
+  struct rlimit rlim;
+  getrlimit(RLIMIT_STACK, &rlim);
+  printf("stack size: %ld\n", (long)rlim.rlim_cur);
+  getrlimit(RLIMIT_NPROC, &rlim);
+  printf("process limit: %ld\n", (long)rlim.rlim_cur);
+  getrlimit(RLIMIT_NOFILE, &rlim);
+  printf("max file descriptors: %ld\n", (long)rlim.rlim_cur);
+  return 0;
 }
